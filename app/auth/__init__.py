6dc308fe333c18fc1cdf7479fd952103e54b234a
philip <philip.haohui.liang@gmail.com>
2016-02-17 13:24:47 +0800

    implemented user register

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -4,4 +4,4 @@ from flask import Blueprint
 auth = Blueprint("auth", __name__)
 
 
-from app.auth import views
+from app.auth import views, forms
--- app/auth/views.py
+++ app/auth/views.py
@@ -4,7 +4,7 @@ from flask.ext.login import login_user, logout_user, login_required
 from app import db
 from app.auth import auth
 from app.models import User
-from app.auth.forms import LoginForm
+from app.auth.forms import LoginForm, RegistrationForm
 
 
 @auth.route("/login", methods=["GET", "POST"])
@@ -28,3 +28,21 @@ def logout():
     logout_user()
     flash("You have been logged out.")
     return redirect(url_for("main.index"))
+
+
+@auth.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+        user = User(
+            email=form.email.data,
+            username=form.username.data,
+            password=form.password.data
+        )
+        db.session.add(user)
+
+        flash("You can now login in.")
+        return redirect(url_for("auth.login"))
+
+    return render_template("auth/register.html", form=form)
